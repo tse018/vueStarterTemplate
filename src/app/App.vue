551425249b2dc1ea92aface1ebc4dcc370fb7832
tse018 <60438717+tse018@users.noreply.github.com>
2021-12-07 14:58:12 +0100

    ready to use

---
+++ src/app/App.vue
@@ -0,0 +1,12 @@
+<template>
+   <Routerview />
+</template>
+
+<script>
+</script>
+
+<style>
+   @import '../style/reset.css';
+   @import '../style/variables.css';
+   @import '../style/style.css';
+</style>
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+import Home from './views/Home.vue';
+
+export default [
+      { name: 'home', path: '/', component:  Home},
+]

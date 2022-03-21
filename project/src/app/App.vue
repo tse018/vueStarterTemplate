30c6700cffca0c49decfa193edee2119de5a73a0
tse018 <60438717+tse018@users.noreply.github.com>
2022-03-21 15:16:09 +0100

    router transition

--- project/src/app/App.vue
+++ project/src/app/App.vue
@@ -1,13 +1,14 @@
 <template>
-   <RouterView />
+   <transition name="animation">
+      <RouterView :key="$route.path" />
+   </transition>
 </template>
 
-<script>
-</script>
+<script></script>
 
 <style>
-   @import '../style/reset.css';
-   @import '../style/font.css';
-   @import '../style/variables.css';
-   @import '../style/style.css';
+@import "../style/reset.css";
+@import "../style/font.css";
+@import "../style/variables.css";
+@import "../style/style.css";
 </style>
